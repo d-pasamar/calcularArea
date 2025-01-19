3c0ca2bb962c0f9d3bebd8f5af72a199484de790
David <david.pasamar@gmail.com>
2025-01-19 14:55:21 +0000

    Se añadieron comentarios para JavaDoc

--- src/figuras/Circunferencia.java
+++ src/figuras/Circunferencia.java
@@ -1,21 +1,44 @@
 package figuras;
 
+/**
+ * La clase Circunferencia representa una circunferencia con los atributos radio y color
+ * Contiene métodos para imprimir en consola su color, diámetro y calcular el área y puede comparar si dos circunferencias son iguales
+ */
 public class Circunferencia {
     private double radio;
     private String color;
 
+    /**
+     * Método que crea una circunferencia con un radio dado
+     *
+     * @param radio El parámetro radio define el radio de la circunferencia
+     */
     public Circunferencia(double radio) {
         this.radio = radio;
     }
 
+    /**
+     * Método que obtiene el radio de la circunferencia
+     *
+     * @return devuelve el radio de la circunferencia
+     */
     public double getRad() {
         return radio;
     }
 
+    /**
+     * Método que establece el radio de la circunferencia
+     *
+     * @param rad El parámetro rad define el radio de la circunferencia
+     */
     public void setRad(double rad) {
         this.radio = rad;
     }
 
+    /**
+     * Método que imprime atributos de la circunferencia:
+     * diámetro, color, área
+     */
     public void imprimir() {
         color = "rojo";
         System.out.println("Di�metro: " + 2 * radio);
@@ -24,6 +47,14 @@ public class Circunferencia {
         System.out.println(area);
     }
 
+    /**
+     * Método que compara dos circunferencias si tienen los radios iguales
+     *
+     * @param considerarDecimales El parámetro considerarDecimales define si tenemos en cuenta los decimales o no; En caso de true, compara ambos radios y nos devuelve true o false;
+     *                            En caso de false, comprueba que la diferencia sin decimales de ambos radios sea inferior a 1 y devuelve true, en caso contrario false
+     * @param otro                El parámetro otro define el radio de la segunda circunferencia
+     * @return devuelve booleano siendo true si las circunferncias son iguales, o tienen una diferencia de radio inferior a 1, si no comparamos con los decimales
+     */
     public boolean esIgual(boolean considerarDecimales, Circunferencia otro) {
         double radio1 = this.radio;
         double radio2 = otro.getRad();
@@ -40,4 +71,3 @@ public class Circunferencia {
         }
     }
 }
-
--- src/figuras/Test.java
+++ src/figuras/Test.java
@@ -1,6 +1,19 @@
 package figuras;
 
+/**
+ * La clase pública Test contiene el método main
+ *
+ * @author David
+ * @version 1.0
+ */
+
 public class Test {
+
+    /**
+     * Método principal del programa para ejecutar la aplicación
+     * Crea tres objetos de la clase Circunferencia y realiza dos comparaciones
+     * Imprime en consola si las circunferencias son iguales según considere los decimales o no
+     */
     public static void main(String[] args) {
         Circunferencia c1 = new Circunferencia(5.5);
         Circunferencia c2 = new Circunferencia(10.1);
